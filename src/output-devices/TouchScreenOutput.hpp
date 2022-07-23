20e42ad9379455bfde8a89bedd20e55724a44490
AhmedSalahAlanani <ahmedsalahalanani@gmail.com>
2022-07-23 20:17:16 +0200

    Add Doxygen comments for output devices

--- src/output-devices/TouchScreenOutput.hpp
+++ src/output-devices/TouchScreenOutput.hpp
@@ -1,17 +1,57 @@
+//!
+//! \file       TouchScreenOutput.hpp
+//! \author     Ahmed Salah Alanani
+//! \date       20-Jul-2022
+//!
+//! \brief      Implementation of the Class TouchScreenOutput
+//!
+
+//---------------------------------------------------------------------------
 #ifndef TOUCH_SCREEN_OUTPUT_HPP
 #define TOUCH_SCREEN_OUTPUT_HPP
+//---------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
 #include "interfaces/IOutputDevice.hpp"
+//---------------------------------------------------------------------------
+// Forward Declarations
+//---------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------
+//!
+//! \namespace embeddedIntentRecognizer
+//!
+//! \brief Embedded Intent Recognizer namespace
+//!
 namespace embeddedIntentRecognizer
 {
+    //---------------------------------------------------------------------------
+    //!
+    //! \class      VoiceOutput
+    //!
+    //! \brief      Class responsible to receive the output from TextProcessor
+    //!             in textual format (which is the recognized intent), and convert
+    //!             it into displayed text on a touch screen.
+    //!
+    //! \note       This class implementation is not done yet but it is currently
+    //!             supported
+    //!
     class TouchScreenOutput : public IOutputDevice
     {
     private:
+        //! IOutputDevice inherited method
         bool init(SupportedLanguages language) override;
+
+        //! IOutputDevice inherited method
         void onNewOutputReady(const std::string &textProcessorOutput) override;
     };
 
 } // namespace embeddedIntentRecognizer
 
 #endif // TOUCH_SCREEN_OUTPUT_HPP
+
+//---------------------------------------------------------------------------
+// End of File
+//---------------------------------------------------------------------------
--- src/output-devices/VoiceOutput.hpp
+++ src/output-devices/VoiceOutput.hpp
@@ -1,17 +1,57 @@
+//!
+//! \file       VoiceOutput.hpp
+//! \author     Ahmed Salah Alanani
+//! \date       20-Jul-2022
+//!
+//! \brief      Implementation of the Class VoiceOutput
+//!
+
+//---------------------------------------------------------------------------
 #ifndef VOICE_OUTPUT_HPP
 #define VOICE_OUTPUT_HPP
+//---------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
 #include "interfaces/IOutputDevice.hpp"
+//---------------------------------------------------------------------------
+// Forward Declarations
+//---------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------
+//!
+//! \namespace embeddedIntentRecognizer
+//!
+//! \brief Embedded Intent Recognizer namespace
+//!
 namespace embeddedIntentRecognizer
 {
+    //---------------------------------------------------------------------------
+    //!
+    //! \class      VoiceOutput
+    //!
+    //! \brief      Class responsible to receive the output from TextProcessor
+    //!             in textual format (which is the recognized intent), and convert
+    //!             it into voice output.
+    //!
+    //! \note       This class implementation is not done yet but it is currently
+    //!             supported
+    //!
     class VoiceOutput : public IOutputDevice
     {
     private:
+        //! IOutputDevice inherited method
         bool init(SupportedLanguages language) override;
+
+        //! IOutputDevice inherited method
         void onNewOutputReady(const std::string &textProcessorOutput) override;
     };
 
 } // namespace embeddedIntentRecognizer
 
 #endif // VOICE_OUTPUT_HPP
+
+//---------------------------------------------------------------------------
+// End of File
+//---------------------------------------------------------------------------
