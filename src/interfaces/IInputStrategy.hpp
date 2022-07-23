8761be48084bca832c2726ea0378f27a186c5285
AhmedSalahAlanani <ahmedsalahalanani@gmail.com>
2022-07-24 00:20:39 +0200

    Add Doxygen comments for the interface classes

--- src/interfaces/IInputStrategy.hpp
+++ src/interfaces/IInputStrategy.hpp
@@ -1,20 +1,86 @@
+//!
+//! \file       IInputStrategy.hpp
+//! \author     Ahmed Salah Alanani
+//! \date       20-Jul-2022
+//!
+//! \brief      Implementation of the Interface IInputStrategy
+//!
+
+//---------------------------------------------------------------------------
 #ifndef I_INPUT_STRATEGY_HPP
 #define I_INPUT_STRATEGY_HPP
+//---------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
 #include <string>
 
 #include "common/Types.hpp"
+//---------------------------------------------------------------------------
+// Forward Declarations
+//---------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------
+//!
+//! \namespace embeddedIntentRecognizer
+//!
+//! \brief Embedded Intent Recognizer namespace
+//!
 namespace embeddedIntentRecognizer
 {
+    //---------------------------------------------------------------------------
+    //!
+    //! \class      IInputStrategy
+    //!
+    //! \brief      The interface shall be responsible for the input strategy
+    //!             handling (i.e input device), where each input strategy shall
+    //!             inherit from this interface and implement the handling of
+    //!             the input according to its format (for example: it could be
+    //!             voice commands, touch screen keyboard, or simple command line
+    //!             interface). Also the input language shall be configurable and
+    //!             set at run time. Additionally, it shall provide user interface
+    //!             if needed, according to the type of input supported.
+    //!
+    //! \note       For all input strategies, the input strategy shall be able to
+    //!             convert the received input and convert it into textual format
+    //!             (no matter its original format is), so that it can be processed
+    //!             by the TextProcessor to recognize the intent of the input
+    //!
     class IInputStrategy
     {
     public:
+        //---------------------------------------------------------------------------
+        //!
+        //! \brief Destructor
+        //!
         virtual ~IInputStrategy() = default;
+        //---------------------------------------------------------------------------
+        //!
+        //! \brief This method is responsible for the initialization of the concrete
+        //!        strategy including the setting of the input language so that
+        //!        the language strategy can be set during initialization.
+        //!
+        //! \param language [in] - the configured input language which shall be supported
+        //!                        by the input strategy
+        //!
         virtual bool init(SupportedLanguages language) = 0;
+        //---------------------------------------------------------------------------
+        //!
+        //! \brief This method polls for input and convert it into textual format
+        //!        if needed and stores it into the string: receivedInput, so that
+        //!        it can be read by the TextProcessor to recognize the intent of the
+        //!        received input
+        //!
+        //! \param receivedInput [out] - the received input in Text format
+        //!
         virtual void waitForInput(std::string &receivedInput) const = 0;
     };
 
 } // namespace embeddedIntentRecognizer
 
 #endif // I_INPUT_STRATEGY_HPP
+
+//---------------------------------------------------------------------------
+// End of File
+//---------------------------------------------------------------------------
--- src/interfaces/IIntentRecognitionModel.hpp
+++ src/interfaces/IIntentRecognitionModel.hpp
@@ -1,18 +1,77 @@
+//!
+//! \file       IIntentRecognitionModel.hpp
+//! \author     Ahmed Salah Alanani
+//! \date       20-Jul-2022
+//!
+//! \brief      Implementation of the Interface IIntentRecognitionModel
+//!
+
+//---------------------------------------------------------------------------
 #ifndef I_INTENT_RECOGNITION_MODEL_HPP
 #define I_INTENT_RECOGNITION_MODEL_HPP
+//---------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
 #include <string>
+//---------------------------------------------------------------------------
+// Forward Declarations
+//---------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------
+//!
+//! \namespace embeddedIntentRecognizer
+//!
+//! \brief Embedded Intent Recognizer namespace
+//!
 namespace embeddedIntentRecognizer
 {
+    //---------------------------------------------------------------------------
+    //!
+    //! \class      IIntentRecognitionModel
+    //!
+    //! \brief      The interface shall be responsible for the intent recognition
+    //!             model, where it shall support loading its DataSet and use it
+    //!             during runtime while running the model and support of course
+    //!             the main functionality of the model which is recognizing
+    //!             the intent of the received text.
+    //!
+    //! \note       Model for each language is assumed to be separate and has
+    //!             its own dataSet
+    //!
     class IIntentRecognitionModel
     {
     public:
+        //---------------------------------------------------------------------------
+        //!
+        //! \brief Destructor
+        //!
         virtual ~IIntentRecognitionModel() = default;
+        //---------------------------------------------------------------------------
+        //!
+        //! \brief This method shall load the Model's DataSet to be used later on
+        //!        during the application runtime to recognize the intent of the input text
+        //!
         virtual bool loadDataSet() = 0;
+        //---------------------------------------------------------------------------
+        //!
+        //! \brief This method shall run the model's main functionality to recognize
+        //!        the intent of the input text. It's expected that the model shall
+        //!        use its previously loaded dataSet
+        //!
+        //! \param text   [in]  - the input text to be processed to recognize its intent
+        //!
+        //! \param intent [out] - the output intent of the input text according to
+        //!                     the model execution
+        //!
         virtual void getIntent(const std::string &text, std::string &intent) const = 0;
     };
 
 } // namespace embeddedIntentRecognizer
 
 #endif // I_INTENT_RECOGNITION_MODEL_HPP
+
+//---------------------------------------------------------------------------
+// End of File
+//---------------------------------------------------------------------------
